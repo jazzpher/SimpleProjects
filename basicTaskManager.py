4fff58a2c65c2f807cd09519cfdcfb12322dd8cb
jazzpher <80498982+jazzpher@users.noreply.github.com>
2023-10-20 20:24:04 +0800

    Create basicTaskManager.py

---
+++ basicTaskManager.py
@@ -0,0 +1,48 @@
+# Define an empty task list
+tasks = []
+
+# Function to add a task
+def add_task(task):
+    tasks.append(task)
+    print("Task added successfully!")
+
+# Function to list all tasks
+def list_tasks():
+    if tasks:
+        print("Tasks:")
+        for i, task in enumerate(tasks, 1):
+            print(f"{i}. {task}")
+    else:
+        print("No tasks found.")
+
+# Function to mark a task as completed
+def complete_task(task_index):
+    if 1 <= task_index <= len(tasks):
+        completed_task = tasks.pop(task_index - 1)
+        print(f"Task '{completed_task}' marked as completed.")
+    else:
+        print("Invalid task index.")
+
+# Main loop
+while True:
+    print("\nTask Manager Menu:")
+    print("1. Add Task")
+    print("2. List Tasks")
+    print("3. Mark Task as Completed")
+    print("4. Exit")
+    
+    choice = input("Enter your choice: ")
+    
+    if choice == '1':
+        task = input("Enter the task description: ")
+        add_task(task)
+    elif choice == '2':
+        list_tasks()
+    elif choice == '3':
+        task_index = int(input("Enter the task index to mark as completed: "))
+        complete_task(task_index)
+    elif choice == '4':
+        print("Goodbye!")
+        break
+    else:
+        print("Invalid choice. Please try again.")
