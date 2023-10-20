243fdb97576b921cefb3c89ffb7e3cc672cf30de
jazzpher <80498982+jazzpher@users.noreply.github.com>
2023-10-20 20:06:25 +0800

    Create simple_calc.py

---
+++ simple_calc.py
@@ -0,0 +1,51 @@
+# Basic Command-Line Calculator in Python
+
+# Function to add two numbers
+def add(x, y):
+    return x + y
+
+# Function to subtract two numbers
+def subtract(x, y):
+    return x - y
+
+# Function to multiply two numbers
+def multiply(x, y):
+    return x * y
+
+# Function to divide two numbers
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero"
+    return x / y
+
+# Main program loop
+while True:
+    print("\nCalculator Menu:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Quit")
+    
+    choice = input("Enter your choice (1/2/3/4/5): ")
+    
+    if choice == '5':
+        print("Goodbye!")
+        break
+    
+    if choice not in ('1', '2', '3', '4'):
+        print("Invalid choice. Please select a valid option.")
+        continue
+    
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    
+    if choice == '1':
+        print(f"Result: {add(num1, num2)}")
+    elif choice == '2':
+        print(f"Result: {subtract(num1, num2)}")
+    elif choice == '3':
+        print(f"Result: {multiply(num1, num2)}")
+    elif choice == '4':
+        result = divide(num1, num2)
+        print(f"Result: {result}")
